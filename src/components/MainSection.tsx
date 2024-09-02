920330e1818ad084c397e9185e0a79b3c300fbf4
Octavio Sanchez <tavowebdev419@gmail.com>
2024-09-02 09:09:16 -0400

    ok

--- src/components/MainSection.tsx
+++ src/components/MainSection.tsx
@@ -8,18 +8,18 @@ export default function MainSection() {
         
             <section className="bg-cyan-900 p-8">
                 <div className="container mx-auto mt-12 mb-12 flex flex-col lg:flex-row items-center justify-center space-y-4 lg:space-y-0 lg:justify-between">
-                    <div className='lg:w-[calc(100%-16px)]'> {/* Adjust width as needed */}
-                    <div className='border-2 border-red-950 p-8 mr-20 rounded-xl'>
+                    <div className='lg:w-[calc(100%-16px)]'> 
+                    <div className='py-8 px-8'>
                         <h1 className="tracking-tighter py-3 px-8 text-center text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-red-50 to-red-300">Quality building materials at wholesale prices!</h1>
-                        <p className="text-center py-3 px-8">
+                        <p className="text-center py-8 px-8">
                             We specialize in providing high-quality solutions tailored to your needs.
                             Whether you&apos;re looking for efficient services or innovative products,
                             we&apos;re here to help you achieve your goals.
                         </p>
                         </div>
                     </div>
-                    <div className='lg:w-[calc(100%-16px)]'> {/* Adjust width as needed */}
-                        <div className='aspect-w-16 aspect-h-9 overflow-hidden rounded-xl'>
+                    <div className='lg:w-[calc(100%-16px)]'>
+                        <div className='aspect-w-16 aspect-h-16 overflow-hidden rounded-xl py-8 px-8'>
                             <video
                                 className="mx-auto aspect-video overflow-hidden object-cover"
                                 controls
